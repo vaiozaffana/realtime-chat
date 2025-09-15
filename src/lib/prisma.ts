d32e1e9b97889298114001c8198a2d9375143a9a
vaiozaffana <vaiozaffana@gmail.com>
2025-09-15 10:33:21 +0700

    chore(env): configure Supabase database environment

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+
+export const prisma =
+globalForPrisma.prisma || new PrismaClient({
+    log: ["error", "warn", "info", "query"],
+});
+// globalForPrisma.prisma || new PrismaClient({
+//     log: ["query", "error", "warn", "info"],
+// });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
